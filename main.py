87e44ebd1ce77648712222d96b04e9f872cab3b1
Shubham <shubham@rklick.com>
2022-09-06 21:24:19 +0530

    created dataframe for video_tbl

--- main.py
+++ main.py
@@ -1,16 +1,74 @@
-# This is a sample Python script.
+import json
+import logging
+import googleapiclient.discovery
+from pytube import YouTube as yt_youtube
+from utils import build_youtube, search_videos, search_comments, process_search_response, process_comments_response, api_key, search_statistics, process_statistics_response
+import pandas as pd
+from db_connection import cursor
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def main(api_key):
+    # READING API and CHANNEL ID from config.json
+
+    # config_file_name = "configvideo_title.json"
+    # with open(config_file_name) as config_file:
+    #    config = json.load(config_file)
+
+    api_key = api_key
+    channel_id = 'UCnz-ZXXER4jOvuED5trXfEA'
+
+    youtube = build_youtube(api_key)
+
+    videos = []
+    comments = []
+    likes = []
+    # try:
+    next_page = None
+    while True:
+        response = search_videos(youtube, channel_id, next_page)
+        next_page, result = process_search_response(response)
+        videos += result
+        if not next_page:
+            break
+    print(videos)
+
+    for video in videos:
+        next_page = None
+        while True:
+            response_comment = search_comments(youtube, video['video_id'], next_page)
+            next_page, result_comments = process_comments_response(response_comment, video)
+            comments += result_comments
+            if not next_page:
+                break
+
+        print("video ===", video)
+        response_likes = search_statistics(youtube, video['video_id'], next_page)
+        print("likes ==", response_likes)
+        result_likes = process_statistics_response(response_likes, video)
+        likes += result_likes
+    # except Exception as e:
+    #     pass
+        # logger.error(f"Error:\n{str(e)}")
+    print(f"Total comments: {len(comments)}")
+    print(f"Total videos: {len(videos)}")
+    print("comments ==", comments)
+    print("likes ==", likes)
+    return comments, likes, videos
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    comments, likes, videos = main(apikey)
+    df_videos = pd.DataFrame.from_records(videos)
+    df_videos['video_link'] = df_videos.apply(lambda row: f"https://www.youtube.com/watch?v={row['video_id']}", axis=1)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    df_likes = pd.DataFrame.from_records(likes)
+    df_merged = pd.merge(df_videos, df_likes[['video_id', 'likes']], on='video_id')
+
+    dict_video = df_merged.T.to_dict().values()
+    for data in dict_video:
+        query = f"insert into youtube_project.video_tbl (video_id, channel_id, video_title, video_link, video_desc, thumb_nail, likes) values('{data['video_id']}', '{data['channel_id']}', '{data['video_title']}', '{data['video_link']}', '{data['video_desc']}', '{data['thumbnail']}', '{data['likes']}')";
+        print(query)
+
+        cursor.execute(query)
+        mydb.commit()
---
+++ utils.py
@@ -0,0 +1,113 @@
+import json
+import googleapiclient.discovery
+import json
+import logging
+from pytube import YouTube as yt_youtube
+
+# api_key = 'AIzaSyCARkUF_v4pNmyQFMbuQ_XBpiuE0ofvD_Y'
+api_key = 'AIzaSyAwjigDbbAlx1boxpbHL08Nx774OsVQyjQ'
+
+
+def build_youtube(API_KEY):
+    api_service_name = "youtube"
+    api_version = "v3"
+    youtube = googleapiclient.discovery.build(
+        api_service_name, api_version, developerKey=API_KEY)
+    return youtube
+
+
+def search_videos(youtube, channel_id, page_token):
+    request = youtube.search().list(
+        part="snippet",
+        channelId=channel_id,
+        type="video",
+        pageToken=page_token
+    )
+    response = request.execute()
+    return response
+
+
+def search_comments(youtube, video_id, page_token):
+    request = youtube.commentThreads().list(
+        part="snippet",
+        videoId=video_id,
+        pageToken=page_token
+    )
+
+    response = request.execute()
+    return response
+
+
+
+def process_search_response(response):
+    next_page_token = response.get('nextPageToken')
+    result = []
+    for i, item in enumerate(response["items"]):
+        video_id = item["id"]["videoId"]
+        video_title = item['snippet']['title']
+        channel_id = item['snippet']['channelId']
+        description = item['snippet']['description']
+        thumbnail = item['snippet']['thumbnails']['default']['url'],
+        video_upld_date = item['snippet']['publishTime']
+        result.append({
+            'video_id': video_id,
+            'video_title': video_title,
+            'channel_id' : channel_id,
+            'video_desc' : description,
+            'thumbnail' : thumbnail[0],
+            'video_upld_date' : video_upld_date
+        })
+    return next_page_token, result
+
+
+def process_comments_response(response, video):
+    next_page_token = response.get('nextPageToken')
+    result = []
+    for i, item in enumerate(response["items"]):
+        comment = item["snippet"]["topLevelComment"]
+        author = comment["snippet"]["authorDisplayName"]  # Use Later
+        comment_text = comment["snippet"]["textDisplay"]
+        video_id = video['video_id']
+        video_title = video['video_title']
+        result.append(
+            {
+                'video_id': video_id,
+                'video_title': video_title,
+                'author': author,
+                'comment_text': comment_text
+            }
+        )
+
+    return next_page_token, result
+
+
+def search_statistics(youtube, video_id, page_token):
+    request = youtube.videos().list(
+        part="snippet, statistics",
+        id=video_id,
+        pageToken=page_token
+    )
+
+    response = request.execute()
+    return response
+
+
+
+
+
+def process_statistics_response(response, video):
+    # next_page_token = response.get('nextPageToken')
+    result = []
+    for i, item in enumerate(response["items"]):
+        likes = item['statistics']['likeCount']
+        video_id = video['video_id']
+        video_title = video['video_title']
+        result.append(
+            {
+                'video_id': video_id,
+                'video_title': video_title,
+                'likes': likes,
+            }
+            )
+
+    return result

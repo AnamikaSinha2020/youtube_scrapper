fbd2e3db32f6bd03f8cbaeda084d56259d3e5db0
Anamika sinha <61617222+AnamikaSinha2020@users.noreply.github.com>
2022-09-11 12:29:05 -0400

    port update

--- app.py
+++ app.py
@@ -88,5 +88,5 @@ def get_cmnt(video_id):
 
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8001, debug=True)
+    app.run(host='0.0.0.0', port=8000, debug=True)
     # app.run(debug=True)

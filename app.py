c8f8a59d73345c850d76cc67d4f7ba962d8ee7fa
Anamika sinha <61617222+AnamikaSinha2020@users.noreply.github.com>
2022-09-11 12:45:32 -0400

    run() updated

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from db_connection import cursor, mydb
 # from urllib.request import urlopen as uReq
 
 app = Flask(__name__)
-app.run(host='0.0.0.0',  debug=True)
+app.run(debug=True)
 
 
 @app.route('/',methods=['GET'])  # route to display the home page

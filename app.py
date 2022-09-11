a1e91f3512f6e01c9f88e5b422124d5d74088b61
Anamika sinha <61617222+AnamikaSinha2020@users.noreply.github.com>
2022-09-11 15:34:23 -0400

    updated

--- app.py
+++ app.py
@@ -91,5 +91,6 @@ def get_cmnt(video_id):
 
 
 if __name__ == "__main__":
-    app.run(host='127.0.0.1', port=8001, debug=True)
+    #app.run(host='127.0.0.1', port=8001, debug=True)
+    app.run()
    
